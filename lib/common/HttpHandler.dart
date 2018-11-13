05525a9ccf9a12e0cb745dabbe1aa301895d9325
unknown <giampieer24@gmail.com>
2018-11-13 14:41:31 -0500

    creacion de class Media.dart

--- lib/common/HttpHandler.dart
+++ lib/common/HttpHandler.dart
@@ -5,19 +5,18 @@ import 'package:http/http.dart' as http;
 
 class HttpHandler {
   String _baseUrl = "http://movies.vrammdev.com";
-  final String _lenguaje = "";
   Future<dynamic>getJson(Uri uri) async{
     http.Response response = await http.get(uri);
-    //llamamos el obejto json de dart   y decodificar
+    //llamamos el objetivo json de dart   y decodificar
     return json.decode(response.body).toString();
   }
   Future<String> fetchMovies(){
     // se concatena la url base , el endpoint (lo restante de la url) y los parametros
-    var uri = new Uri.http( 'movies.vrammdev.com', '', {
+    var uri = new Uri.http( _baseUrl , '', {
       'op' : '3',
     });
-    print(uri);
-    //then recivimos el resultado o un error
+    //print(uri);
+    //then recibimos el resultado o un error
     return getJson(uri).then(((data) => data.toString()));
   }
 }
---
+++ lib/model/Media.dart
@@ -0,0 +1,21 @@
+class Media {
+  String url;
+  String title;
+  String gender1;
+  String image;
+  //ejemplo de u arraya en json
+  List<dynamic>genreIds;
+
+  factory Media(Map jsonMap) {
+    return new Media.deserialize(jsonMap);
+  }
+  //vamos a capturar los valores del json
+  //deserialize = es tranformar un json en un objeto
+  //serializar = es tomar las propiedad los valores y transformar en un archivo json o xml
+  Media.deserialize(Map json) :
+        url = json["url"].toString(),
+        title = json["title"].toString(),
+        gender1 = json["gender1"].toString(),
+        image = json["image"].toString(),
+        genreIds = json["genreIds"].toList();
+}
