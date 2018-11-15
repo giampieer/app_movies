2e0c9bcadf10dcfd9fca4c23163300ae02126604
unknown <giampieer24@gmail.com>
2018-11-15 11:16:53 -0500

    modificacion de tamaños de la imagen API

--- lib/media_list.dart
+++ lib/media_list.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/common/HttpHandler.dart';
 import 'package:movies_app/model/Media.dart';
+import 'package:movies_app/media_list_item.dart';
 
 class MediaList  extends StatefulWidget {
   @override
@@ -28,12 +29,8 @@ class _MediaListSate extends State<MediaList> {
       child: new  ListView.builder(
         itemCount : _media.length ,
         itemBuilder: (BuildContext context, int index) {
-          return new Column(
-            children: <Widget>[
-              //se llama el metodo de la imagen de la class Media.dart
-              new Image.network(_media[index].getPosterUrl())
-            ],
-          );
+          //se llama el copnstructor de la clase media_list_item
+          return new MediaListItem(_media[index]);
         },
       ),
     );
--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -1,17 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:movies_app/model/Media.dart';
 class MediaListItem extends StatelessWidget {
+  //creamos un objeto
   final  Media media;
+  //creacion de un constructor que viaje el objeto media
   MediaListItem(this.media);
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+    //crea un widget Card
     return new Card(
       child: new Column(
         children: <Widget>[
           new FadeInImage.assetNetwork(
-              placeholder: "assets/placeholder.jpg",
-              image: media.getPosterUrl())
+            //se usa placehorlder si una imagen no carga de la API
+            placeholder: "assets/placeholder.jpg",
+            image: media.getPosterUrl(),
+            //como se va desplegar la imagen dento de los diferentes espacio ubicados
+            fit: BoxFit.cover,
+            //definir el ancho de la imagen es inifito (ocupa todo el espacio
+            width: double.infinity,
+            //definir el tamaño de la imagen
+            height: 400.0,
+            //duracion de la animacion de la imagen
+            fadeInDuration: new Duration(milliseconds: 40),
+          )
         ],
       ),
     );
