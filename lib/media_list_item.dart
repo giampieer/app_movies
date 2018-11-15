5e25feb0682c3d1b58124f8bd532179dea8672a3
unknown <giampieer24@gmail.com>
2018-11-15 11:30:34 -0500

    creacion de la flanja

--- lib/media_list_item.dart
+++ lib/media_list_item.dart
@@ -12,18 +12,41 @@ class MediaListItem extends StatelessWidget {
     return new Card(
       child: new Column(
         children: <Widget>[
-          new FadeInImage.assetNetwork(
-            //se usa placehorlder si una imagen no carga de la API
-            placeholder: "assets/placeholder.jpg",
-            image: media.getPosterUrl(),
-            //como se va desplegar la imagen dento de los diferentes espacio ubicados
-            fit: BoxFit.cover,
-            //definir el ancho de la imagen es inifito (ocupa todo el espacio
-            width: double.infinity,
-            //definir el tamaño de la imagen
-            height: 400.0,
-            //duracion de la animacion de la imagen
-            fadeInDuration: new Duration(milliseconds: 40),
+          //se crea un contenedor que envuelva el card
+          // para la creacion de la flanja de informacion de cada imagen
+          Container(
+            child: new Stack(
+               children: <Widget>[
+                 new FadeInImage.assetNetwork(
+                   //se usa placehorlder si una imagen no carga de la API
+                   placeholder: "assets/placeholder.jpg",
+                   image: media.getPosterUrl(),
+                   //como se va desplegar la imagen dento de los diferentes espacio ubicados
+                   fit: BoxFit.cover,
+                   //definir el ancho de la imagen es inifito (ocupa todo el espacio
+                   width: double.infinity,
+                   //definir el tamaño de la imagen
+                   height: 400.0,
+                   //duracion de la animacion de la imagen
+                   fadeInDuration: new Duration(milliseconds: 40),
+                 ),
+                 //se usa para posicionar la flanja
+                 new Positioned(
+                   //no tenga espacios (bordes ) las flanjas
+                   left: 0.0,
+                   bottom: 0.0,
+                   right: 0.0,
+                   child: new Container(
+                     decoration: new BoxDecoration(
+                       color: Colors.grey[900].withOpacity(0.5),
+                     ),
+                     constraints: new BoxConstraints(
+                       maxHeight: 55.0,
+                     ),
+                   ),
+                 )
+               ],
+            ),
           )
         ],
       ),
